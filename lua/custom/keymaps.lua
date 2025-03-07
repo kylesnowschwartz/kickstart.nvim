851da03dc335fa0e2006c602020c6fce6d541a1a
kylesnowschwartz <kyle.snowschwartz@gmail.com>
2025-03-07 12:15:31 +1100

    feature(custom keymaps): Add custom keymapping configurations
    This commit introduces a new Lua configuration file `custom/keymaps.lua`
    that defines various key mappings for Neovim. The key mappings cover multiple categories:
    
    - **Terminal Commands**: Key mappings allowing exit from terminal mode using leader+t and Esc. For instance, `<leader><Esc><Esc>` closes the terminal.
    
    - **Buffer Commands**: Mappings related to buffer management like delete (`<leader>bd`), navigation between buffers (`<leader>bn`, `<leader>bp`), and reload current buffer (`<leader>bR`).
    
    - **Toggle Features**: Enable toggling of line numbers (`<leader>tn`) and line wrapping (`<leader>tl`) for enhanced flexibility in code editing.
    
    - **Error Navigation**: Key bindings (<leader>en, <leader>ep) make it easy to traverse through the location list of errors or warnings.
    
    - **Miscellaneous Shortcuts**: Includes actions such as re-indenting buffer content (`<leader>=`), clearing search highlights (`<leader>sc`), and optimizing tab focus for buffer switching by setting `<Tab>` to `'<C-^>'`.
    
    Each key mapping is described with a descriptive name and intended action, aiming to streamline navigation and operation within Neovim. The provided configurations improve productivity by enhancing accessibility and reducing keystroke requirements through thoughtfully assembled bindings.

---
+++ lua/custom/keymaps.lua
@@ -0,0 +1,102 @@
+-- Custom Keymaps
+--
+----------------------------------------------------------------------------------
+-- TERMINAL COMMANDS (leader + t)
+--------------------------------------------------------------------------------
+-- vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>:bd!<CR>', { desc = 'Close terminal' })
+vim.keymap.set('t', '<leader><Esc><Esc>', '<C-\\><C-n>:bd!<CR>', { desc = 'Close terminal' })
+
+--------------------------------------------------------------------------------
+-- BUFFER COMMANDS (leader + b)
+--------------------------------------------------------------------------------
+-- Delete (kill) current buffer
+vim.keymap.set('n', '<leader>bd', ':bdelete<CR>', { desc = '[B]uffer [d]elete' })
+
+-- Go to next/previous buffer
+vim.keymap.set('n', '<leader>bn', ':bn<CR>', { desc = '[B]uffer [n]ext' })
+vim.keymap.set('n', '<leader>bp', ':bp<CR>', { desc = '[B]uffer [p]revious' })
+
+-- Reload current buffer (like re-edit)
+vim.keymap.set('n', '<leader>bR', ':edit<CR>', { desc = '[B]uffer [R]eload current file' })
+
+--------------------------------------------------------------------------------
+-- FILE COMMANDS (leader + f)
+--------------------------------------------------------------------------------
+-- Edit Neovim config (replace with your actual init.lua path, if desired)
+vim.keymap.set('n', '<leader>fed', ':e $MYVIMRC<CR>', { desc = 'open init.lua' })
+
+-- Reloading not supported with lazy.nvim
+-- vim.keymap.set('n', '<leader>feR', ':source $MYVIMRC', { desc = 'reload init.lua' })
+
+-- Prompt to open/edit a new file (substituting current directory path, etc.)
+-- TODO: Figure out find file vs format
+-- vim.keymap.set('n', '<leader>ff', ':edit <C-R>=substitute(expand("%:p:h"), $HOME, "~", "")<CR>/', { desc = '[F]ile [f]ind file prompt' })
+
+-- Write (save) current file
+vim.keymap.set('n', '<leader>fs', ':write<CR>', { desc = '[F]ile [s]ave' })
+
+-- Write all open files
+vim.keymap.set('n', '<leader>fS', ':wall<CR>', { desc = '[F]ile save [A]ll' })
+
+--------------------------------------------------------------------------------
+-- QUITTING (leader + q)
+--------------------------------------------------------------------------------
+-- Quit all
+vim.keymap.set('n', '<leader>qq', ':quitall<CR>', { desc = '[Q]uit [a]ll' })
+
+-- Quit all (discard changes)
+vim.keymap.set('n', '<leader>qQ', ':quitall!<CR>', { desc = '[Q]uit all (force)' })
+
+-- Save all and quit
+vim.keymap.set('n', '<leader>qs', ':xa<CR>', { desc = '[Q]uit & [s]ave all' })
+
+--------------------------------------------------------------------------------
+-- WINDOW / SPLITS (leader + w)
+--------------------------------------------------------------------------------
+-- Horizontal / vertical splits
+vim.keymap.set('n', '<leader>w-', ':split<CR>', { desc = '[W]indow - horizontal split' })
+vim.keymap.set('n', '<leader>w/', ':vsplit<CR>', { desc = '[W]indow / vertical split' })
+
+-- Equalize split sizes
+vim.keymap.set('n', '<leader>w=', '<C-W>=', { desc = '[W]indow resize [=]' })
+
+-- Close current split
+vim.keymap.set('n', '<leader>wd', ':q<CR>', { desc = '[W]indow [d]elete' })
+
+-- Jump among splits
+vim.keymap.set('n', '<leader>wh', '<C-W>h', { desc = '[W]indow left' })
+vim.keymap.set('n', '<leader>wj', '<C-W>j', { desc = '[W]indow down' })
+vim.keymap.set('n', '<leader>wk', '<C-W>k', { desc = '[W]indow up' })
+vim.keymap.set('n', '<leader>wl', '<C-W>l', { desc = '[W]indow right' })
+
+-- Switch split windows (rotate focus)
+vim.keymap.set('n', '<leader>ww', '<C-W><C-W>', { desc = '[W]indow cycle' })
+
+-- Maximize current windows
+vim.keymap.set('n', '<leader>wm', ':only<CR>', { desc = '[W]indow cycle' })
+--------------------------------------------------------------------------------
+-- TOGGLES (leader + t)
+--------------------------------------------------------------------------------
+-- Toggle line numbers
+vim.keymap.set('n', '<leader>tn', ':set number!<CR>', { desc = '[T]oggle [n]umber' })
+
+-- Toggle line wrapping
+vim.keymap.set('n', '<leader>tl', ':set wrap!<CR>', { desc = '[T]oggle [l]ine wrap' })
+
+--------------------------------------------------------------------------------
+-- ERROR/LOCATION LIST NAVIGATION (leader + e)
+--------------------------------------------------------------------------------
+vim.keymap.set('n', '<leader>en', ':lnext<CR>', { desc = '[E]rror [n]ext (location list)' })
+vim.keymap.set('n', '<leader>ep', ':lprev<CR>', { desc = '[E]rror [p]revious (location list)' })
+
+--------------------------------------------------------------------------------
+-- MISC
+--------------------------------------------------------------------------------
+-- Re-indent entire buffer
+vim.keymap.set('n', '<leader>=', 'mzgg=G`z', { desc = 'Reindent entire buffer' })
+
+-- Clear search highlight
+vim.keymap.set('n', '<leader>sc', ':nohlsearch<CR>', { desc = '[S]earch [c]lear highlight' })
+
+-- (Optionally) if you still want <Tab> to switch buffers:
+-- vim.keymap.set('n', '<Tab>', '<C-^>', { desc = 'Switch to previous buffer' })
