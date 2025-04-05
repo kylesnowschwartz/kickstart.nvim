69bdace73a36f2b1962ed65bba634f0a8b7871e8
kylesnowschwartz <kyle.snowschwartz@gmail.com>
2025-04-06 11:44:52 +1200

    Feat: Add Neogit, a Magit inspired nvim plugin <3

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -107,4 +107,18 @@ return {
       mappings = '<leader>gyy',
     },
   },
+  {
+    'NeogitOrg/neogit',
+    dependencies = {
+      'nvim-lua/plenary.nvim', -- required
+      'sindrets/diffview.nvim', -- optional - Diff integration
+      'nvim-telescope/telescope.nvim',
+    },
+    config = true,
+    keys = {
+      { '<leader>gG', '<cmd>Neogit cwd=%:p:h<cr>', desc = 'Neogit (cwd)' },
+      { '<leader>gg', '<cmd>Neogit<cr>', desc = 'Neogit (project)' },
+      { '<leader>gl', '<cmd>Neogit log<cr>', desc = 'Neogit Log (project)' },
+    },
+  },
 }
