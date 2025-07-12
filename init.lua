ebfa13c5d17169aba6b5eb790b244437d42d4b85
kylesnowschwartz <kyle.snowschwartz@gmail.com>
2025-07-12 20:59:48 +1200

    Feat: Ensure when nvim closes, we reset terminal to avoid UI bugs

--- init.lua
+++ init.lua
@@ -147,6 +147,15 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Reset terminal when leaving Neovim
+vim.api.nvim_create_autocmd('VimLeave', {
+  desc = 'Reset terminal when leaving Neovim',
+  group = vim.api.nvim_create_augroup('reset-terminal', { clear = true }),
+  callback = function()
+    vim.cmd 'silent !reset'
+  end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
