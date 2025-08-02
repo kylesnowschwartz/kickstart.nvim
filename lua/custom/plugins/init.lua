432cc5bd6feedae691b30173738f984cc593754b
kylesnowschwartz <kyle.snowschwartz@gmail.com>
2025-08-02 22:30:57 +1200

    Feat: Add mcphub initial install

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -344,4 +344,14 @@ return {
       }
     end,
   },
+  {
+    'ravitemer/mcphub.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    build = 'npm install -g mcp-hub@latest', -- Installs `mcp-hub` node binary globally
+    config = function()
+      require('mcphub').setup()
+    end,
+  },
 }
